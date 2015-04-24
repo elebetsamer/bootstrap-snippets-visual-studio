8a8c7f7f4d501a6ee21ed792f14facc928242b0b
Eric Lebetsamer <elebet@gmail.com>
2015-04-24 12:08:42 -0700

    Update build script
    Use glob instead of fs.readdir and filter in order to get file list.
    Don't use async on xml2js parser. With the newer version of xml2js,
    async caused the parser to fail.

--- build.js
+++ build.js
@@ -1,81 +1,76 @@
 var fs = require('fs'),
-    path = require('path'),
-    util = require('util'),
-    xml2js = require('xml2js'),
-    handlebars = require('handlebars');
+	util = require('util'),
+	xml2js = require('xml2js'),
+	handlebars = require('handlebars'),
+	glob = require("glob");
 
 fs.createReadStream('LICENSE').pipe(fs.createWriteStream('BootstrapSnippets/LICENSE.txt'));
 
-var parser = new xml2js.Parser({async: true, explicitRoot: false, explicitArray: false, ignoreAttrs: false});
+var parser = new xml2js.Parser({
+	async: false,
+	explicitRoot: false,
+	explicitArray: false,
+	ignoreAttrs: false
+});
 
-fs.readdir(__dirname + '/BootstrapSnippets/Snippets/HTML/Bootstrap/', function (err, files) {
-    if (err) {
-        throw err;
-    }
+glob(__dirname + '/BootstrapSnippets/Snippets/HTML/Bootstrap/*.snippet', function (err, files) {
+	var snippets = [];
 
-    var filteredFiles = files.filter(function (file) {
-            return path.extname(file) == '.snippet';
-        }),
-        snippets = [];
+	files.forEach(function (file) {
+		fs.readFile(file, {encoding: 'utf8'}, function (err, data) {
+			parser.parseString(data, function (err, result) {
+				var snippet = {
+					'file': file,
+					'title': result.CodeSnippet.Header.Title,
+					'shortcut': result.CodeSnippet.Header.Shortcut,
+					'description': result.CodeSnippet.Header.Description,
+					'language': result.CodeSnippet.Snippet.Code.$.Language,
+					'declarations': []
+				}
 
-    filteredFiles.forEach(function (file) {
-        //console.log(file);
-        fs.readFile(__dirname + '/BootstrapSnippets/Snippets/HTML/Bootstrap/' + file, function (err, data) {
-            parser.parseString(data, function (err, result) {
-                var snippet = {
-                    'file': file,
-                    'title': result.CodeSnippet.Header.Title,
-                    'shortcut': result.CodeSnippet.Header.Shortcut,
-                    'description': result.CodeSnippet.Header.Description,
-                    'language': result.CodeSnippet.Snippet.Code.$.Language,
-                    'declarations': []
-                }
+				if (result.CodeSnippet.Snippet.Declarations && result.CodeSnippet.Snippet.Declarations.Literal) {
+					if (util.isArray(result.CodeSnippet.Snippet.Declarations.Literal)) {
+						result.CodeSnippet.Snippet.Declarations.Literal.forEach(function (declaration) {
+							snippet.declarations[snippet.declarations.length] = {
+								'id': declaration.ID,
+								'tooltip': declaration.ToolTip,
+								'default': declaration.Default
+							};
+						});
+					} else {
+						snippet.declarations[snippet.declarations.length] = {
+							'id': result.CodeSnippet.Snippet.Declarations.Literal.ID,
+							'tooltip': result.CodeSnippet.Snippet.Declarations.Literal.ToolTip,
+							'default': result.CodeSnippet.Snippet.Declarations.Literal.Default
+						};
+					}
+				}
 
-                if (result.CodeSnippet.Snippet.Declarations && result.CodeSnippet.Snippet.Declarations.Literal) {
-                    if (util.isArray(result.CodeSnippet.Snippet.Declarations.Literal)) {
-                        result.CodeSnippet.Snippet.Declarations.Literal.forEach(function (declaration) {
-                            snippet.declarations[snippet.declarations.length] = {
-                                'id': declaration.ID,
-                                'tooltip': declaration.ToolTip,
-                                'default': declaration.Default
-                            };
-                        });
-                    } else {
-                        snippet.declarations[snippet.declarations.length] = {
-                            'id': result.CodeSnippet.Snippet.Declarations.Literal.ID,
-                            'tooltip': result.CodeSnippet.Snippet.Declarations.Literal.ToolTip,
-                            'default': result.CodeSnippet.Snippet.Declarations.Literal.Default
-                        };
-                    }
-                }
+				snippets[snippets.length] = snippet;
 
-                snippets[snippets.length] = snippet;
-
-                if (snippets.length == filteredFiles.length) {
-                    processSnippets(snippets);
-                }
-            });
-        });
-    });
-
-
+				if (snippets.length == files.length) {
+					processSnippets(snippets);
+				}
+			});
+		});
+	});
 });
 
 function processSnippets(snippets) {
-    snippets.sort(function (a, b) {
-        var keyA = a.shortcut,
-            keyB = b.shortcut;
+	snippets.sort(function (a, b) {
+		var keyA = a.shortcut,
+			keyB = b.shortcut;
 
-        if (keyA < keyB) return -1;
-        if (keyA > keyB) return 1;
+		if (keyA < keyB) return -1;
+		if (keyA > keyB) return 1;
 
-        return 0;
-    });
+		return 0;
+	});
 
-    fs.readFile("snippet-listing-markdown.handlebars", function (err, data) {
-        var template = handlebars.compile(data.toString());
-        var markdown = template({snippets: snippets});
+	fs.readFile("snippet-listing-markdown.handlebars", function (err, data) {
+		var template = handlebars.compile(data.toString());
+		var markdown = template({snippets: snippets});
 
-        fs.writeFile("snippet-listing.md", markdown);
-    });
+		fs.writeFile("snippet-listing.md", markdown);
+	});
 }
